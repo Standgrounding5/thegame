7e278353c86c91ef7c878ec416ce34f2d30043e9
Standgrounding5 <stand9916@gmail.com>
2019-10-06 13:34:37 +0300

    added source files Part 2
    Added

---
+++ projectile.js
@@ -0,0 +1,10 @@
+function Bullet(float speed, float angle, int damage){
+    this.speed = speed;
+    //will come at speed with an angle to X axis(use setinterval function)
+    //and deal "damage" damage
+    this.damage = damage;
+}
+
+function Missile(float speed, float angle, int damage, int radius){
+    //basically a bullet but it explodes?
+}
