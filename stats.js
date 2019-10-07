0634050e86e955255990ac746b70bd371f0e582f
Standgrounding5 <stand9916@gmail.com>
2019-10-07 19:39:38 +0300

    Create stats.js

---
+++ stats.js
@@ -0,0 +1,22 @@
+var stats = {
+    PISTOLMAN : {
+        HP : 100,
+        Type : "Infantry",
+        Damage : 20,
+        Range : 4
+    },
+    RIFLEMAN : {},
+    SHOTGUNMAN : {},
+    MACHINEGUNNER : {},
+    DEMOLITIONER : {},
+    APC : {},
+    TANK : {},
+    HEAVYTANK : {},
+    LIGHTTANK : {},
+    TRUCK : {},
+    FIGHTER : {},
+    BOMBER : {},
+    FMOTORCYCLE : {},
+    AMOTORCYCLE : {}
+    //more to come
+}
